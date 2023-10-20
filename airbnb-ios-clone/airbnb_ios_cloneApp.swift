5fa4260ddfe7147adf688550483dd292d88bd589
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-20 16:35:01 -0700

    Initial Commit

---
+++ airbnb-ios-clone/airbnb_ios_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnb_ios_cloneApp.swift
+//  airbnb-ios-clone
+//
+//  Created by jackson mowatt gok on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct airbnb_ios_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
