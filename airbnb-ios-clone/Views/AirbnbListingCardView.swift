f333cb71a1355beec80de6439f0e9a14052ca73e
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-22 14:44:12 -0700

    images formatted correctly with text and description

--- airbnb-ios-clone/Views/AirbnbListingCardView.swift
+++ airbnb-ios-clone/Views/AirbnbListingCardView.swift
@@ -14,10 +14,21 @@ struct AirbnbListingCardView: View {
     var body: some View {
         HStack {
             AsyncImage(url: URL(string: model.thumbnail_url ?? ""))
+                .frame(width: 100, height: 100)
+                .scaledToFit()
+                .clipped()
             
-            Text(model.name ?? "Listing")
-                .font(.title)
-                .bold()
+            VStack {
+                Text(model.name ?? "Listing")
+                    .lineLimit(1)
+                    .font(.title3)
+                    .bold()
+                
+                Text(model.description ?? "Listing")
+//                    .foregroundColor(Color(.secondaryLabel))
+                    .lineLimit(3)
+                    .font(.body)
+            }
         }
     }
 }
